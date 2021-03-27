165db1e7c5335c7e1a3960f3f5c2b3ebdbd4c1f7
chandrans <chandrans@CHANDRAPF28R7MB>
2021-03-27 22:20:15 +0530

    test2

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ public class Main {
 	public static void main(String[] args) {
            System.out.println("Test Git integration with junkins");
            System.out.println("test 2");
+           System.out.println("test 3");
 	}
 
 }

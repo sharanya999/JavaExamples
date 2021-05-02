49a526d9e46cd6c8daa1175257a0ffa5cd365237
chandrans <chandrans@CHANDRAPF28R7MB>
2021-05-02 18:38:50 +0530

    add new logic

--- src/A.java
+++ src/A.java
@@ -4,6 +4,8 @@ public class A {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
               System.out.println("AAAAAAAAA");
+              System.out.println("BBBBBBBBBBB");
+
 	}
 
 }
